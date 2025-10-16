ba10ac1d90bf67951e131fb9a69af602f8c10030
Infinite Dao <hunter_of_the_world@mail.ru>
2025-10-16 17:17:48 +0300

    Add: added new service

---
+++ device-edge-service/src/main/java/org/iot_platform/deviceedgeservice/DeviceEdgeServiceApplication.java
@@ -0,0 +1,13 @@
+package org.iot_platform.deviceedgeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceEdgeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeviceEdgeServiceApplication.class, args);
+    }
+
+}
---
+++ device-edge-service/src/test/java/org/iot_platform/deviceedgeservice/DeviceEdgeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.iot_platform.deviceedgeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviceEdgeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
